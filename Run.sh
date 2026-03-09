64738c7bcad8cba69f5fa6c7a4ec7b20545e37dc
CaDoCoda <dyhn55c6r4@privaterelay.appleid.com>
2026-03-08 23:07:46 -0600

    Add Run.sh to execute the Swift script

---
+++ Run.sh
@@ -0,0 +1,10 @@
+echo' #!/bin/bash
+
+# Make the Swift script executable
+chmod +x run-apple.swift
+
+# Make this script executable (harmless if already executable)
+chmod +x run.sh
+
+# Run the Swift script
+./run-apple.swift
